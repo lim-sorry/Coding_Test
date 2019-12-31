5c6b7000ccc022da4629e8f140f802fe4a8e6eb3
prime18k <58265373+prime18k@users.noreply.github.com>
2019-12-31 13:11:18 +0900

    Delete Programmers_가운데 글자 가져오기.py

--- Programmers_Level1/Programmers_가운데 글자 가져오기.py
+++
@@ -1,8 +0,0 @@
-from math import floor
-
-def solution(s):
-    if len(s) % 2 == 1:
-        answer = s[floor(len(s)/2)]
-    else:
-        answer = s[int(len(s)/2-1):int(len(s)/2+1)]
-    return answer
