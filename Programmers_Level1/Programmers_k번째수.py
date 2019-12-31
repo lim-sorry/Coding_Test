16fd50475d9d7ee58d2e16c5e1a97432cd0f3aa0
prime18k <58265373+prime18k@users.noreply.github.com>
2019-12-31 13:11:16 +0900

    Delete Programmers_k번째수.py

--- Programmers_Level1/Programmers_k번째수.py
+++
@@ -1,7 +0,0 @@
-def solution(array, commands):
-    answer = []
-    for i in range(len(commands)):
-        cut_array = array[commands[i][0]-1:commands[i][1]]
-        cut_array.sort()
-        answer.append(cut_array[commands[i][2]-1])
-    return answer
